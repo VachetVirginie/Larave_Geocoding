3ee6640f5b102852af538ab2ec4e949f9aeea8ef
Vachet Virginie <vachetvirginie@gmail.com>
2018-04-28 12:08:16 +0200

    initialisation bdd

---
+++ geocoding/database/migrations/2018_04_28_100346_create_points_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->float('lat');
+            $table->float('lng');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('points');
+    }
+}
